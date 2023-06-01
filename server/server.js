934f8d05ec0ae55b24c7cb4534bfe5c7ce605107
JayceHammond <hjayce6@gmail.com>
2023-06-01 13:57:05 -0500

    Base react app redone

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const port = 5000
+
+app.get("/api", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
+})
+
+app.listen(port, () => {console.log("Server started on port " + port)})
